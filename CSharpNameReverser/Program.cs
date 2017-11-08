d77d99e0563febfa54fbf62e877362f6ead631cf
Khanh Nguyen <con.nguyen.86@gmail.com>
2017-11-07 20:36:25 -0800

    complete

--- CSharpNameReverser/Program.cs
+++ CSharpNameReverser/Program.cs
@@ -10,6 +10,21 @@ namespace CSharpNameReverser
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your name.");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reverse name: " + reversed);
+        }
+        // for practice with procedural programming, we separate the console procedures with the method procedures
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+
         }
     }
 }
